5b085115b2e9c9935b8ed87bf1bdf722095fba08
Wishmouse <heidi.goosen@gmail.com>
2018-10-26 07:05:40 +1300

    letters colouring red when value is zero

--- public/javascript/client.js
+++ public/javascript/client.js
@@ -9,12 +9,15 @@ var request = require('superagent')
 
   $('#alphabet').delegate('.letters', 'click', function(e){
            var ab = $(this).attr('id')
+           var newClass = $(this).attr('class', 'used_letters');
            alphabet =ab.toUpperCase()
 
             var letter1 = $('#letter1').val()
             var letter2 = $('#letter2').val()
             var letter3 = $('#letter3').val()
             var letter4 = $('#letter4').val()
+
+
 
           if(letter1 == ""){
             $('#letter1').val(alphabet)
@@ -43,13 +46,12 @@ var request = require('superagent')
        //if statement for error/ response
         })
         charArray()
-        appendRow()
         removeWord()
 
      })
 
      function charArray(){
-       var mysteryWord = 'QRTY'
+       var mysteryWord = 'ROSE'
        var letter1 = $('#letter1').val()
        var letter2 = $('#letter2').val()
        var letter3 = $('#letter3').val()
@@ -59,9 +61,19 @@ var request = require('superagent')
          alert ('boom!  you are amazing')
        } else {
            var mysteryArray = mysteryWord.split('')
-           var wordArray = word.split('')
-           var filteredLetters = mysteryArray.filter((word) => !wordArray.includes(word));
-           filteredLetters.length
+           var guessArray = word.split('')
+           var filteredKeywords = guessArray.filter((word) => !mysteryArray.includes(word));
+            xtotal = filteredKeywords.length-4
+            total = xtotal*-1
+            if (total >0 ){
+              totalSpan ="" +
+                 "<span class='past_input' style='background: #bdf5bd' type='text'>"+total+"</span>"
+            } else {
+              totalSpan ="" +
+               "<span class='past_input' style='background: #f5bdbd' type='text'>"+total+"</span>" }
+
+ console.log(total)
+
 
            var pastWords = $("#past_words")
            var pastWordAttempts ="" +
@@ -70,14 +82,28 @@ var request = require('superagent')
               "<span class='past_input' type='text'>"+letter2+"</span>" +
               "<span class='past_input' type='text'>"+letter3+"</span>" +
               "<span class='past_input' type='text'>"+letter4+"</span>" +
-              "<span class='past_input' type='text'>"+filteredLetters.length+"</span>" +
+              totalSpan+
             "</div>"
             pastWords.append(pastWordAttempts)
+
+            if(total == 0){
+              $( ".past_input" ).each(function( i ) {
+                  past_input_value = $( ".past_input" ).html()
+
+                  if ( past_input_value == letter1 ||past_input_value == letter2 || past_input_value == letter3 || past_input_value == letter4) {
+                    this.style.color = "blue";
+                  } else {
+                    this.style.color = "orange";
+                  }
+                });
+            }
+            removeWord()
          }
        }
 
 
      function removeWord(){
+       $('.used_letters').removeClass('used_letters').addClass('letters')
        $('#letter1').val('')
        $('#letter2').val('')
        $('#letter3').val('')
